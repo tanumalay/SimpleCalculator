47acdc8d1de7db438e5271bb018a2b3079eac273
tanumalay <59199778+tanumalay@users.noreply.github.com>
2020-05-10 23:40:51 +0530

    Update app.py
    exception handling added

--- app.py
+++ app.py
@@ -7,25 +7,31 @@ app = Flask(__name__)
 def home_page():
     return render_template('index.html')
 
+
 @app.route('/math', methods=['POST'])  # This will be called from UI
 def math_operation():
-    if (request.method=='POST'):
-        operation=request.form['operation']
-        num1=int(request.form['num1'])
-        num2 = int(request.form['num2'])
+    try:
+        if (request.method=='POST'):
+            operation=request.form['operation']
+            num1=int(request.form['num1'])
+            num2=int(request.form['num2'])
+    except ValueError:
+        return render_template('results.html', result="Please supply valid inputs and try again.")
+    else:
         if(operation=='add'):
             r=num1+num2
-            result= 'the sum of '+str(num1)+' and '+str(num2) +' is '+str(r)
+            result= 'The sum of '+str(num1)+' and '+str(num2) +' is '+str(r)
         if (operation == 'subtract'):
             r = num1 - num2
-            result = 'the difference of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+            result = 'The difference of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
         if (operation == 'multiply'):
             r = num1 * num2
-            result = 'the product of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+            result = 'The product of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
         if (operation == 'divide'):
             r = num1 / num2
-            result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
+            result = 'The quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
         return render_template('results.html',result=result)
+
 
 @app.route('/via_postman', methods=['POST']) # for calling the API from Postman/SOAPUI
 def math_operation_via_postman():
